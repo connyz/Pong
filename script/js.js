dc7064ea5280211bad6596630905a9f4f46ff7c7
connyz <connymartinsson@yahoo.se>
2014-12-06 15:10:01 +0100

    Got functioning collision detect with walls. Now for paddles.

--- script/js.js
+++ script/js.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	// Main game function
+	// Main game class
 	function Game(){
 		// Gamefield vars
 		this.fieldH = $(".field").css("height");
@@ -15,14 +15,30 @@ $(function(){
 		// Ball speed and movement variables
 		this.ballXVelocity = 2;
 		this.ballYVelocity = 4;
-		this.ballMovingRight = Math.round(Math.random());
-		this.ballMovingUp = 1; /*Math.round(Math.random());*/
+		this.ballMovingRight = 0; //Math.round(Math.random());
+		this.ballMovingUp = 1; //Math.round(Math.random());
 
 		// Spawn the ball
 		this.spawnBall();
 
-		// Start moving ball at random direction
-		this.moveBall();
+		// Draw up paddles
+		this.createPaddles();
+
+		// Start the game
+		this.Start();
+	};
+
+	Game.prototype.Start = function (){
+		// Keep track of this
+		var self = this;
+		// Main interval
+		setInterval(function(){
+			// Start moving ball at random direction
+			self.moveBall();
+
+			// Check for collision with walls
+			self.collisionDetection();
+		},25);
 	};
 
 	Game.prototype.spawnBall = function (){
@@ -40,56 +56,89 @@ $(function(){
 		// Keep track of this
 		var self = this;
 
-		setInterval(function(){
-			//Start moving depending och false or true directions
-			if (self.ballMovingUp){
-				var ballPos = $(".ball").css("top");
-				ballPos = parseInt(ballPos);
-				var tempPos = ballPos -= self.ballYVelocity;
-				$(".ball").css("top", tempPos);
-			}
+		//Start moving depending och false or true directions
+		//console.log("setinterval firing");
 
-			if (this.ballMovingUp){
-				this.ballY += this.ballYVelocity;
-			}
+		// If bMU true then
+		if (self.ballMovingUp){
+			var ballYPos = $(".ball").css("top");
+			ballYPos = parseInt(ballYPos, 10);
+			var tempPos = ballYPos -= self.ballYVelocity;
+			$(".ball").css("top", tempPos);
+		}
 
-			if (this.ballMovingRight){
-				this.ballX += this.ballXVelocity;
-			}
+		// If bMU NOT true then
+		if (!self.ballMovingUp){
+			var ballYPos = $(".ball").css("top");
+			ballYPos = parseInt(ballYPos, 10);
+			var tempPos = ballYPos += self.ballYVelocity;
+			$(".ball").css("top", tempPos);
+		}
 
-			if (this.ballMovingRight){
-				this.ballX += this.ballXVelocity;
-			}
+		// If bMR true then
+		if (self.ballMovingRight){
+			var ballXPos = $(".ball").css("left");
+			ballXPos = parseInt(ballXPos, 10);
+			var tempPos = ballXPos += self.ballXVelocity;
+			$(".ball").css("left", tempPos);
+		}
 
-			if (!this.ballMovingUp){
-				this.ballY -= this.ballYVelocity;
-			}
-
-			if (!this.ballMovingUp){
-				this.ballY -= this.ballYVelocity;
-			}
-
-			if (!this.ballMovingRight){
-				this.ballX -= this.ballXVelocity;
-			}
-
-			if (!this.ballMovingRight){
-				this.ballX -= this.ballXVelocity;
-			}
-		},1025);
-
-		// Check for touching colliding with any walls
-
-	};
-
-	Game.prototype.countScores = function (){
-
+		// If bMR NOT true then
+		if (!self.ballMovingRight){
+			var ballXPos = $(".ball").css("left");
+			ballXPos = parseInt(ballXPos, 10);
+			var tempPos = ballXPos -= self.ballXVelocity;
+			$(".ball").css("left", tempPos);
+		}
 	};
 
 	Game.prototype.collisionDetection = function (){
+		// Keep track of this
+		var self = this;
 
+		// Get ball Y position
+		var tempBallYPos = parseInt($(".ball").css("top"),10);
+		// Get ball X position
+		var tempBallXPos = parseInt($(".ball").css("left"),10);
+
+		//console.log(tempBallYPos + " " + tempBallXPos);
+
+		//Check for collision against upper wall
+		if(tempBallYPos <= 0){
+			self.ballMovingUp = 0;
+		}
+
+		//Check for collision against lower wall
+		if(tempBallYPos >= parseInt(self.fieldH,10)-parseInt(self.ballH,10)){
+			self.ballMovingUp = 1;
+		}
+
+		//Check for collision against right wall
+		if(tempBallXPos <= 0){
+			console.log("COLLIDED LEFT WALL");
+			// Set ballMovingRight to "true"
+			self.ballMovingRight = 1;
+
+			// Scoring and restart placed here later, also check for finishing score (5 points?)
+		}
+
+		//Check for collision against left wall
+		if(tempBallXPos >= parseInt(self.fieldW,10)-parseInt(self.ballW,10)){
+			console.log("COLLIDED RIGHT WALL");
+			// Set ballMovingRight to "false"
+			self.ballMovingRight = 0;
+		}
+
+		// Scoring and restart placed here later, also check for finishing score (5 points?)
 	};
 
+	Game.prototype.countScores = function (){
+		// Keep track of this
+		var self = this;
+	};
+
+
+	// Start the game
 	var test = new Game();
 
 	//console.log(test.ballY);
